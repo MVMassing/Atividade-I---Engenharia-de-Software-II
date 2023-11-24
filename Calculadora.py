ca7d8bf703aa5c758e50a06c104be64377878d12
Matheus <matheus_massing@hotmail.com>
2023-11-23 21:22:37 -0300

    Corrige conflitos de versionamento e adicionando métodos para calcular a potência e raiz quadrada

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,5 @@
+import math
+
 def add(x, y):
     return x + y
 
@@ -8,32 +10,43 @@ def multiply(x, y):
     return x * y
 
 def divide(x, y):
-    if y != 0:
-        return x / y
-    else:
-        return "Erro: Divisão por zero"
+    return x / y if y != 0 else "Erro: Divisão por zero"
+
+def power(x, y):
+    return x ** y
+
+def square_root(x):
+    return math.sqrt(x) if x >= 0 else "Erro: Raiz quadrada de número negativo"
+
+def get_user_input(message):
+    return float(input(message))
 
 def calculator():
+    operations = {
+        '1': add,
+        '2': subtract,
+        '3': multiply,
+        '4': divide,
+        '5': power,
+        '6': square_root
+    }
+
     print("Selecione a operação:")
-    print("1. Soma")
-    print("2. Subtração")
-    print("3. Multiplicação")
-    print("4. Divisão")
+    for key, value in operations.items():
+        print(f"{key}. {value.__name__.capitalize()}")
 
-    choice = input("Digite a opção (1/2/3/4): ")
+    choice = input("Digite a opção (1/2/3/4/5/6): ")
 
-    num1 = float(input("Digite o primeiro número: "))
-    num2 = float(input("Digite o segundo número: "))
+    if choice in operations:
+        num1 = get_user_input("Digite o primeiro número: ")
 
-    if choice == '1':
-        print(num1, "+", num2, "=", add(num1, num2))
-    elif choice == '2':
-        print(num1, "-", num2, "=", subtract(num1, num2))
-    elif choice == '3':
-        print(num1, "*", num2, "=", multiply(num1, num2))
-    elif choice == '4':
-        print(num1, "/", num2, "=", divide(num1, num2))
+        if choice != '6':
+            num2 = get_user_input("Digite o segundo número: ")
+
+        result = operations[choice](num1, num2) if choice != '6' else operations[choice](num1)
+        print(f"Resultado: {result}")
     else:
         print("Opção inválida")
 
-calculator()
+if __name__ == "__main__":
+    calculator()
